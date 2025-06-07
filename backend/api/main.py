83196fcc113b2bb6ddac173f26ec4ebc573ed880
西瓜° <wxh_email@yeah.net>
2025-06-07 16:49:19 +0800

    dev

--- backend/api/main.py
+++ backend/api/main.py
@@ -7,4 +7,4 @@ from api.routes import openapi
 api_router = APIRouter()
 api_router.include_router(info.router,prefix="/api",tags=["info"])
 api_router.include_router(calc.router,prefix="/api",tags=["calc"])
-api_router.include_router(openapi.router,prefix="/api/openapi",tags=["openapi"])
+api_router.include_router(openapi.router,prefix="/api/open",tags=["openapi"])
