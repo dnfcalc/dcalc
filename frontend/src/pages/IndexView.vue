36b7de2fab0d38d69188dc8f59fc4ffed201e46e
西瓜° <wxh_email@yeah.net>
2025-06-15 23:13:23 +0800

    dev

--- frontend/src/pages/IndexView.vue
+++ frontend/src/pages/IndexView.vue
@@ -8,7 +8,10 @@
 </template>
 
 <script lang="ts" name="index" setup>
-const zoom = computed(() => /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent || navigator.vendor || (window as any).opera) ? 1 :(window.screen.width / 1800 / 0.85).toFixed(2))
+const zoom = computed(() => {
+  const DPI = /macintosh|mac os x/i.test(navigator.userAgent.toLowerCase()) ? 1.25 : window.devicePixelRatio || 1;
+  return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent || navigator.vendor || (window as any).opera) ? 1 :(window.screen.width / 1800 / 0.85 / DPI).toFixed(2)
+})
 import { useDialog } from '@/components/hooks/dialog'
 
 const { render } = useDialog()
