b67a3d832642faef7c4adc2bd6b21e4968b7ede0
西瓜° <wxh_email@yeah.net>
2025-06-20 10:53:28 +0800

    dev

--- frontend/src/pages/IndexView.vue
+++ frontend/src/pages/IndexView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full overflow-y-auto mt-0 h-100%" :style="`zoom:${zoom}`">
+  <div class="w-full mt-0 h-100%" :style="`zoom:${zoom}`">
     <Suspense>
       <RouterView></RouterView>
     </Suspense>
@@ -9,8 +9,8 @@
 
 <script lang="ts" name="index" setup>
 const zoom = computed(() => {
-  const DPI = Math.max(0.85,Math.min(/macintosh|mac os x/i.test(navigator.userAgent.toLowerCase()) ? 1 : window.devicePixelRatio || 1,2)) * 1.0
-  return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent || navigator.vendor || (window as any).opera) ? 1 :(window.screen.width / 1800 / DPI).toFixed(2)
+  const DPI = Math.max(0.85,Math.min(/macintosh|mac os x/i.test(navigator.userAgent.toLowerCase()) ? 1 : window.devicePixelRatio || 1,2)) * 0.95
+  return Math.max(1,/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent || navigator.vendor || (window as any).opera) ? 1 :(window.screen.width / 1800 / DPI))
 })
 import { useDialog } from '@/components/hooks/dialog'
 
