fbfce35c02ac6016c21ffb90f2da7154fe03fda4
西瓜° <wxh_email@yeah.net>
2025-06-29 23:55:33 +0800

    dev

--- backend/core/character/adventure.py
+++ backend/core/character/adventure.py
@@ -172,7 +172,7 @@ adv = [
         'children': [
             {'id': 0, 'name': 'vanguard', 'title': '千魂·征战者', 'open': False},
             {'id': 1, 'name': 'skirmisher', 'title': '千魂·决战者', 'open': False},
-            {'id': 2, 'name': 'dragoon', 'title': '千魂·狩猎者', 'open': False},
+            {'id': 2, 'name': 'dragoon', 'title': '千魂·狩猎者', 'open': True, 'class': 'GF.demonic_lancer.dragoon'},
             {'id': 3, 'name': 'impaler', 'title': '千魂·暗枪士', 'open': False},
             {'id': 4, 'name': 'empty', 'title': ''},
         ],
