3148d9a7b88f57694267bbb5619ef690bdecd9de
西瓜° <wxh_email@yeah.net>
2026-04-09 16:05:49 +0800

    dev

---
+++ frontendNew/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <Button>Hello world</Button>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
